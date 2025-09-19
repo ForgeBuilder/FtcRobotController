345775df96ae46d2fec86e523b9a26d79aadfaf8
ForgeBuilder <elliotkarlson@gmail.com>
2025-09-19 17:02:28 -0500

    Old hide
    put all the old code in a package, hidden from everything else.
    
    Also, the into the deep folder will be deleted after this.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep/BasketAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep/BasketAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.IntoTheDeep;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep/DriveForwardTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep/DriveForwardTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.IntoTheDeep;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.Servo;
