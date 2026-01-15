dc2f12e2d30839cb551f84d71dc8ef786a0cb2db
ForgeBuilder <elliotkarlson@gmail.com>
2026-01-15 11:09:53 -0600

    LaunchCoder updates
    trying to tune the launcher PID and get it consistent
    
    Some ideas for shot tracking
    
    I need to find a way to track shots..
    
    I'll know we've taken all the shots if the intake motor is spinning at max speed, or if it's been a certain duration, or if I track the blips in the turret's RPM.
    
    I'll do a simple timer for starters (for auto, doesn't matter for teleop)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/CrossbowMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/CrossbowMain.java
@@ -232,6 +232,7 @@ public class CrossbowMain extends OpMode {
 
     public static int max_average_error = 15;
     public static int max_current_error = 20; //there is no 30 so this is goofy but whatever
+    public static int max_current_error_lazy = 40; //there is no 30 so this is goofy but whatever
 
     //how fast can the robot be rotating and still fire?
     double max_angular_velocity = 10;
@@ -342,7 +343,14 @@ public class CrossbowMain extends OpMode {
             rightBack.setPower(rightBack.getPower()-chasis_aim_turn);//-(zero_power_turn*cats));
 
             //take the shot - once you've started, don't stop!
-            boolean open_door_conditions = ((speed_ready && ((angular_velocity_acceptable && limelight_ready) || override_shot)));
+
+            boolean left_speed_met_easy = Math.abs(launcherSpeed + left_current_speed) < max_current_error_lazy;
+            boolean right_speed_met_easy = Math.abs(launcherSpeed - right_current_speed) < max_current_error_lazy;
+            boolean basic_speed_met = left_speed_met_easy&&right_speed_met_easy;
+            
+
+            boolean open_door_conditions = ((speed_ready && ((angular_velocity_acceptable && limelight_ready) || (override_shot && basic_speed_met))));
+            //If the speed goes back down.. too bad. door stays open.
             boolean keep_door_open = open_door && fire;
 
             open_door = open_door_conditions || keep_door_open;
@@ -353,6 +361,7 @@ public class CrossbowMain extends OpMode {
                 door_open_timer.reset();
             }
         } else {
+            launcher_freeze_movement = false;
             spin_launcher = false;
             trying_to_fire = false;
             open_door = false;
