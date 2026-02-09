96869cf132884df481ec42fd9de855f27520272b
ForgeBuilder <elliotkarlson@gmail.com>
2026-02-08 18:07:45 -0600

    Basic swerve test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SwerveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SwerveTest.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -18,32 +17,62 @@ public class SwerveTest extends OpMode {
     double large_diff_bevel_teeth = 64;
     double motor_gear_teeth = 15;
     double small_diff_bevel_teeth = 18;
-    private DcMotorEx swerve1;
-    private DcMotorEx swerve2;
+
+    //motors delcared in order they are plugged into the controller left to right, control hub to expansion hub
+    private DcMotorEx l_swerve_up;
+    private DcMotorEx l_swerve_down;
+    private DcMotorEx r_swerve_up;
+    private DcMotorEx r_swerve_down;
+
 
     @Override public void init(){
 
-        swerve1 = hardwareMap.get(DcMotorEx.class,"swerve 1");
-        swerve2 = hardwareMap.get(DcMotorEx.class,"swerve 2");
+        l_swerve_up = hardwareMap.get(DcMotorEx.class,"LSwerveUp");
+        l_swerve_down = hardwareMap.get(DcMotorEx.class,"LSwerveDown");
+
+        r_swerve_up = hardwareMap.get(DcMotorEx.class,"RSwerveUp");
+        r_swerve_down = hardwareMap.get(DcMotorEx.class,"RSwerveDown");
+
+
+
 
         //reset encoders
-        swerve1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        swerve2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        swerve1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        swerve2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        l_swerve_up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        l_swerve_down.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        l_swerve_up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        l_swerve_down.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        r_swerve_up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        r_swerve_down.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        r_swerve_up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        r_swerve_down.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        r_swerve_up.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        r_swerve_down.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        r_swerve_up.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        r_swerve_down.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override public void loop(){
-        swerve1.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
-        swerve2.setPower(gamepad1.left_stick_y-gamepad1.right_stick_x);
+        double translational_x = gamepad1.left_stick_x;
+        double translational_y = gamepad1.left_stick_y;
 
-        double average_encoder_position = (swerve1.getCurrentPosition()+swerve2.getCurrentPosition())/2;
+        double yaw = gamepad1.right_stick_x;
 
-        double rotation_encoder_ratio = (motor_gear_teeth/large_diff_bevel_teeth)/(Encoder_resolution);
 
-        rotation = average_encoder_position*rotation_encoder_ratio;
 
-        rotation = average_encoder_position*rotation_encoder_ratio;
+        l_swerve_up.setPower(-gamepad1.left_stick_y+gamepad1.right_stick_x);
+        l_swerve_down.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
+
+        r_swerve_up.setPower(-gamepad1.left_stick_y+gamepad1.right_stick_x);
+        r_swerve_down.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
+
+        double r_average_encoder_position = (l_swerve_up.getCurrentPosition()+ l_swerve_down.getCurrentPosition())/2;
+        double r_rotation_encoder_ratio = (motor_gear_teeth/large_diff_bevel_teeth)/(Encoder_resolution);
+
+        rotation = r_average_encoder_position*r_rotation_encoder_ratio;
+
+        rotation = r_average_encoder_position*r_rotation_encoder_ratio;
 
         telemetry.addData("pod rotation: ",rotation);
     }
