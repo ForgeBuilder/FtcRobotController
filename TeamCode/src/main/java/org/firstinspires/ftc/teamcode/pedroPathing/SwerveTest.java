3cd46571b9207a231cbdc08f2c0e46560113b574
ForgeBuilder <elliotkarlson@gmail.com>
2026-02-08 19:19:22 -0600

    swerve directional
    swerve points wheel where I tell it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SwerveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SwerveTest.java
@@ -4,12 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
+import org.firstinspires.ftc.teamcode.pedroPathing.PID;
 
 @TeleOp(name="SwerveTest")
 public class SwerveTest extends OpMode {
 
-    double rotation = 0;
+    double l_rotation = 0;
     double Encoder_resolution = 537.7; //ticks per revolution
 
     //gear ratio constants
@@ -23,7 +23,6 @@ public class SwerveTest extends OpMode {
     private DcMotorEx l_swerve_down;
     private DcMotorEx r_swerve_up;
     private DcMotorEx r_swerve_down;
-
 
     @Override public void init(){
 
@@ -53,27 +52,70 @@ public class SwerveTest extends OpMode {
         r_swerve_down.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
+    public static PID l_rotation_pid = new PID(5,0,0);
+
+    private double l_rotation_target_jumps = 0; //This will be increments of 0.5. could be an int but im lazy with type conversions.
+
     @Override public void loop(){
+
+        //gear math
+        double r_average_encoder_position = (l_swerve_up.getCurrentPosition()+ l_swerve_down.getCurrentPosition())/2;
+        double r_rotation_encoder_ratio = (motor_gear_teeth/large_diff_bevel_teeth)/(Encoder_resolution);
+
+
+        //translation and rotation desires
         double translational_x = gamepad1.left_stick_x;
         double translational_y = gamepad1.left_stick_y;
 
         double yaw = gamepad1.right_stick_x;
 
+        double start_rotation = -0.25;
+
+        //1 Swerve pod code
+
+        l_rotation = start_rotation + r_average_encoder_position*r_rotation_encoder_ratio; //This is in 360 turns, not radians.
+
+        double l_forward = 0;
+
+        double l_desired_rotation_turns; //turns is the unit
+
+        if ((Math.abs(translational_y)+Math.abs(translational_x))>0){ //Math.pow and sqrt for length but we don't need that so this is easier on the computer
+            l_desired_rotation_turns = Math.atan2(translational_y,translational_x)/(Math.PI*2);
+        } else {
+            l_desired_rotation_turns = l_rotation;
+        }
+
+        //avoids turning more than we need. Should ensure forward always faces where we want! 0.5 means the wheel allways faces forward, with 0.25 we can reverse the wheel and it can be w
+        if ((l_rotation-(l_desired_rotation_turns+l_rotation_target_jumps))>0.5){
+            l_rotation_target_jumps += 1;
+        }
+        if ((l_rotation-(l_desired_rotation_turns+l_rotation_target_jumps))<-0.5){
+            l_rotation_target_jumps -= 1;
+        }
 
 
-        l_swerve_up.setPower(-gamepad1.left_stick_y+gamepad1.right_stick_x);
-        l_swerve_down.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
+        double l_turn = l_rotation_pid.update(l_desired_rotation_turns+l_rotation_target_jumps,l_rotation); //This is in 360 turns, not radians.
+        ; //clockwise/right +
 
-        r_swerve_up.setPower(-gamepad1.left_stick_y+gamepad1.right_stick_x);
-        r_swerve_down.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
+        double l_swerve_up_power = -l_forward+l_turn;
+        double l_swerve_down_power = l_forward+l_turn;
 
-        double r_average_encoder_position = (l_swerve_up.getCurrentPosition()+ l_swerve_down.getCurrentPosition())/2;
-        double r_rotation_encoder_ratio = (motor_gear_teeth/large_diff_bevel_teeth)/(Encoder_resolution);
+        l_swerve_up.setPower(l_swerve_up_power);
+        l_swerve_down.setPower(l_swerve_down_power);
+        telemetry.addData("Top MotorPower:",l_swerve_up_power);
+        telemetry.addData("Bottom MotorPower:",l_swerve_down_power);
 
-        rotation = r_average_encoder_position*r_rotation_encoder_ratio;
 
-        rotation = r_average_encoder_position*r_rotation_encoder_ratio;
 
-        telemetry.addData("pod rotation: ",rotation);
+        telemetry.addData("pod rotation: ", l_rotation);
+
+
+
+//
+//        double r_forward = 0;
+//        double r_turn = 0; //clockwise/right +
+//
+//        r_swerve_up.setPower(-r_forward+r_turn);
+//        r_swerve_down.setPower(r_forward+r_turn);
     }
 }
