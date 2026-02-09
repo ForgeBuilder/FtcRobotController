8333f580fbb786db5584864d312872e8e73b6294
ForgeBuilder <elliotkarlson@gmail.com>
2026-02-08 22:05:31 -0600

    First attempt at class strucutre

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SwerveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SwerveTest.java
@@ -4,40 +4,161 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.pedroPathing.PID;
 
 @TeleOp(name="SwerveTest")
 public class SwerveTest extends OpMode {
 
-    double l_rotation = 0;
-    double Encoder_resolution = 537.7; //ticks per revolution
+
+    ///swerve related mechanical constants
+    double Encoder_resolution = 537.7;
 
     //gear ratio constants
+    double motor_gear_teeth = 15;
     double large_diff_spur_teeth = 66;
     double large_diff_bevel_teeth = 64;
-    double motor_gear_teeth = 15;
     double small_diff_bevel_teeth = 18;
 
-    //motors delcared in order they are plugged into the controller left to right, control hub to expansion hub
-    private DcMotorEx l_swerve_up;
-    private DcMotorEx l_swerve_down;
-    private DcMotorEx r_swerve_up;
-    private DcMotorEx r_swerve_down;
+    ///swerves
+    SwervePod leftSwerve;
+    SwervePod rightSwerve;
+
+    ///swerve class definition VV
+    public class SwervePod {
+
+        ///Optional or can't provide at start VV
+        double heading = 0;
+        double start_rotation = -0.25; //If your pod is starting facing forwards, use this. 1 "turn" = 360 degrees
+        double center_x_offset = 0;
+        double center_y_offset = 0;
+
+        ///Must provide to work VV
+
+        //motors
+        double Encoder_resolution; //ticks per revolution
+        private DcMotorEx up_motor;
+        private DcMotorEx down_motor;
+
+        //gears
+        double motor_gear_teeth = 15;
+        double large_diff_spur_teeth = 66;
+        double large_diff_bevel_teeth = 64;
+        double small_diff_bevel_teeth = 18;
+
+        double rotation_encoder_ratio;
+
+        ///Constructor(s)
+
+        //create during init() -- pretty please
+        public SwervePod(
+            double Encoder_resolution,
+            DcMotorEx up_motor,
+            DcMotorEx down_motor,
+            double motor_gear_teeth,
+            double large_diff_spur_teeth,
+            double large_diff_bevel_teeth,
+            double small_diff_bevel_teeth
+        ){
+            //rotation (yaw) will not work if you do not provide a center
+
+            this.Encoder_resolution = Encoder_resolution;
+            this.up_motor = up_motor;
+            this.down_motor = down_motor;
+            this.motor_gear_teeth = motor_gear_teeth;
+            this.large_diff_spur_teeth = large_diff_spur_teeth;
+            this.large_diff_bevel_teeth = large_diff_bevel_teeth;
+            this.small_diff_bevel_teeth = small_diff_bevel_teeth;
+            this.rotation_encoder_ratio = (motor_gear_teeth/large_diff_spur_teeth)/(Encoder_resolution);
+
+            up_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            down_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            up_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            down_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+        ///Methods
+
+        public void setHeadingZero(){
+            up_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            down_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            up_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            down_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        public void updatePod(double translational_y,double translational_x, double yaw){
+            //1 Swerve pod code
+
+            //gear math operations
+            double r_average_encoder_position = (up_motor.getCurrentPosition()+ down_motor.getCurrentPosition())/2;
+
+            heading = start_rotation + r_average_encoder_position* rotation_encoder_ratio; //This is in 360 turns, not radians.
+
+            double l_forward = 0;
+
+            if ((Math.abs(translational_y)+Math.abs(translational_x))>0) { //Math.pow and sqrt for length but we don't need that so this is easier on the computer
+                l_desired_rotation_turns = Math.atan2(translational_y,translational_x)/(Math.PI*2);
+            } else {
+                l_desired_rotation_turns = heading;
+            }
+
+            //avoids turning more than we need. Should ensure forward always faces where we want! 0.5 means the wheel allways faces forward, with 0.25 we can reverse the wheel and it can be w
+            boolean condition_greater = true;
+            while (condition_greater){
+                condition_greater = (heading -(l_desired_rotation_turns+l_rotation_target_jumps))>0.25;
+                if (condition_greater){
+                    l_rotation_target_jumps += 0.5;
+                }
+            }
+            boolean condition_lesser = true;
+            while (condition_lesser){
+                condition_lesser = (heading -(l_desired_rotation_turns+l_rotation_target_jumps))<-0.25;
+                if (condition_lesser){
+                    l_rotation_target_jumps -= 0.5;
+                }
+//                telemetry.addData("changing this!","true");
+            }
+
+            boolean drive_flip; //is the drive wheel 180 the wrong way and we need to drive reverse?
+
+            if (Math.abs(l_rotation_target_jumps) != Math.floor(Math.abs(l_rotation_target_jumps))){
+                drive_flip = true;
+            } else {
+                drive_flip = false;
+            }
+//            telemetry.addData("flip: ",drive_flip);
+
+            double turn = l_rotation_pid.update(l_desired_rotation_turns+l_rotation_target_jumps, heading); //This is in 360 turns, not radians.
+            ; //clockwise/right +
+//            telemetry.addData("l_desired_rotation_turns",l_desired_rotation_turns);
+//            telemetry.addData("l_rotation_target_jumps",l_rotation_target_jumps);
+
+            double up_power = -l_forward+turn;
+            double down_power = l_forward+turn;
+
+            up_motor.setPower(up_power);
+            down_motor.setPower(down_power);
+//            telemetry.addData("Top MotorPower:",up_power);
+//            telemetry.addData("Bottom MotorPower:",down_power);
+//            telemetry.addData("pod rotation: ", heading);
+        }
+    }
+
+    //End of swerve definition related things
+    //
+    //
+    /// Opmode stuff!
 
     @Override public void init(){
 
-        l_swerve_up = hardwareMap.get(DcMotorEx.class,"LSwerveUp");
-        l_swerve_down = hardwareMap.get(DcMotorEx.class,"LSwerveDown");
 
-        r_swerve_up = hardwareMap.get(DcMotorEx.class,"RSwerveUp");
-        r_swerve_down = hardwareMap.get(DcMotorEx.class,"RSwerveDown");
+        leftSwerve = new SwervePod(
+                Encoder_resolution,
+                hardwareMap.get(DcMotorEx.class,"LSwerveUp"),
+                hardwareMap.get(DcMotorEx.class,"LSwerveDown"),
+                motor_gear_teeth,
+                large_diff_spur_teeth,
+                small_diff_bevel_teeth,
+                small_diff_bevel_teeth
+        );
 
-
-
-        r_swerve_up.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        r_swerve_down.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        r_swerve_up.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        r_swerve_down.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public static PID l_rotation_pid = new PID(5,0.2,0);
@@ -48,114 +169,20 @@ public class SwerveTest extends OpMode {
 
     @Override public void init_loop(){
         if (gamepad1.xWasPressed()){
-//            reset encoders
-            l_swerve_up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            l_swerve_down.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            l_swerve_up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            l_swerve_down.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            r_swerve_up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            r_swerve_down.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            r_swerve_up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            r_swerve_down.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftSwerve.setHeadingZero();
         }
+        //leftSwerve.updatePod(0,0,0);
+
+        //don't do this ^^ because zero power breaking? unsure. TBD later.
     }
 
     @Override public void loop(){
-
-        if (gamepad1.xWasPressed()){
-//            reset encoders
-        l_swerve_up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        l_swerve_down.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        l_swerve_up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        l_swerve_down.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        r_swerve_up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        r_swerve_down.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        r_swerve_up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        r_swerve_down.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-
-
-        //gear math
-        double r_average_encoder_position = (l_swerve_up.getCurrentPosition()+ l_swerve_down.getCurrentPosition())/2;
-        double r_rotation_encoder_ratio = (motor_gear_teeth/large_diff_spur_teeth)/(Encoder_resolution);
-
-
         //translation and rotation desires
         double translational_x = gamepad1.left_stick_x;
         double translational_y = gamepad1.left_stick_y;
 
         double yaw = gamepad1.right_stick_x;
 
-        double start_rotation = -0.25;
-
-        //1 Swerve pod code
-
-        l_rotation = start_rotation + r_average_encoder_position*r_rotation_encoder_ratio; //This is in 360 turns, not radians.
-
-        double l_forward = 0;
-
-        if ((Math.abs(translational_y)+Math.abs(translational_x))>0) { //Math.pow and sqrt for length but we don't need that so this is easier on the computer
-            l_desired_rotation_turns = Math.atan2(translational_y,translational_x)/(Math.PI*2);
-        } else {
-            l_desired_rotation_turns = l_rotation;
-        }
-
-        //avoids turning more than we need. Should ensure forward always faces where we want! 0.5 means the wheel allways faces forward, with 0.25 we can reverse the wheel and it can be w
-        boolean condition_greater = true;
-        while (condition_greater){
-            condition_greater = (l_rotation-(l_desired_rotation_turns+l_rotation_target_jumps))>0.25;
-            if (condition_greater){
-                l_rotation_target_jumps += 0.5;
-            }
-        }
-        boolean condition_lesser = true;
-        while (condition_lesser){
-             condition_lesser = (l_rotation-(l_desired_rotation_turns+l_rotation_target_jumps))<-0.25;
-             if (condition_lesser){
-                 l_rotation_target_jumps -= 0.5;
-             }
-             telemetry.addData("changing this!","true");
-        }
-
-        boolean drive_flip; //is the drive wheel 180 the wrong way and we need to drive reverse?
-
-        if (Math.abs(l_rotation_target_jumps) != Math.floor(Math.abs(l_rotation_target_jumps))){
-            drive_flip = true;
-        } else {
-            drive_flip = false;
-        }
-        telemetry.addData("flip: ",drive_flip);
-
-
-
-
-        double l_turn = l_rotation_pid.update(l_desired_rotation_turns+l_rotation_target_jumps,l_rotation); //This is in 360 turns, not radians.
-        ; //clockwise/right +
-        telemetry.addData("l_desired_rotation_turns",l_desired_rotation_turns);
-        telemetry.addData("l_rotation_target_jumps",l_rotation_target_jumps);
-
-
-        double l_swerve_up_power = -l_forward+l_turn;
-        double l_swerve_down_power = l_forward+l_turn;
-
-        l_swerve_up.setPower(l_swerve_up_power);
-        l_swerve_down.setPower(l_swerve_down_power);
-        telemetry.addData("Top MotorPower:",l_swerve_up_power);
-        telemetry.addData("Bottom MotorPower:",l_swerve_down_power);
-
-
-
-        telemetry.addData("pod rotation: ", l_rotation);
-
-
-
-//
-//        double r_forward = 0;
-//        double r_turn = 0; //clockwise/right +
-//
-//        r_swerve_up.setPower(-r_forward+r_turn);
-//        r_swerve_down.setPower(r_forward+r_turn);
+        leftSwerve.updatePod(translational_x,translational_y,yaw);
     }
 }
