8eb8125e0f20bc09c1cacbcee58abf38f5a03ac5
ForgeBuilder <elliotkarlson@gmail.com>
2026-03-04 16:06:41 -0600

    TURRET IS STABLE, unit test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BallistaPedroUnitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BallistaPedroUnitTest.java
@@ -20,20 +20,20 @@ public class BallistaPedroUnitTest extends CrossbowMain {
 
     @Override public void start(){
         super.start();
-        follower.setMaxPower(0.3);
-        test_path = follower.pathBuilder()
-                .addPath(new BezierLine(pose_a, pose_b))
-                .setLinearHeadingInterpolation(pose_a.getHeading(), pose_b.getHeading(), 0.5)
-                .addPath(new BezierLine(pose_b, pose_c))
-                .setConstantHeadingInterpolation(pose_c.getHeading())
-                .addPath(new BezierLine(pose_c, pose_d))
-                .setLinearHeadingInterpolation(pose_c.getHeading(), pose_d.getHeading(), 0.5)
-                .addPath(new BezierLine(pose_d, pose_a))
-                .setConstantHeadingInterpolation(pose_d.getHeading())
-                .build();
     }
     @Override public void loop(){
         if (!follower.isBusy()){
+            follower.setMaxPower(0.3);
+            test_path = follower.pathBuilder()
+                    .addPath(new BezierLine(pose_a, pose_b))
+                    .setLinearHeadingInterpolation(pose_a.getHeading(), pose_b.getHeading(), 0.5)
+                    .addPath(new BezierLine(pose_b, pose_c))
+                    .setConstantHeadingInterpolation(pose_c.getHeading())
+                    .addPath(new BezierLine(pose_c, pose_d))
+                    .setLinearHeadingInterpolation(pose_c.getHeading(), pose_d.getHeading(), 0.5)
+                    .addPath(new BezierLine(pose_d, pose_a))
+                    .setConstantHeadingInterpolation(pose_d.getHeading())
+                    .build();
             follower.followPath(test_path);
         }
     }
